3153cfc005b360f857744d7fd80aed7fc5f9f9c6
Omarb28 <omarb28@gmail.com>
2021-07-24 08:25:44 +0300

    Modify code style to adhere to PEP8

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -3,9 +3,9 @@ from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_filename = "database.db"
+db_filename = "database.db"
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+db_path = "sqlite:///{}".format(os.path.join(project_dir, db_filename))
 
 db = SQLAlchemy()
 
@@ -16,7 +16,7 @@ setup_db(app)
 
 
 def setup_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
@@ -26,7 +26,8 @@ def setup_db(app):
 db_drop_and_create_all()
     drops the database tables and starts fresh
     can be used to initialize a clean database
-    !!NOTE you can change the database_filename variable to have multiple verisons of a database
+    !!NOTE you can change the database_filename variable
+        to have multiple verisons of a database
 '''
 
 
@@ -39,23 +40,22 @@ def db_drop_and_create_all():
         recipe='[{"name": "water", "color": "blue", "parts": 1}]'
     )
 
-
     drink.insert()
 # ROUTES
 
-'''
-Drink
-a persistent drink entity, extends the base SQLAlchemy Model
-'''
-
 
 class Drink(db.Model):
+    '''
+    Drink
+    a persistent drink entity, extends the base SQLAlchemy Model
+    '''
     # Autoincrementing, unique primary key
     id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
     # String Title
     title = Column(String(80), unique=True)
     # the ingredients blob - this stores a lazy json blob
-    # the required datatype is [{'color': string, 'name':string, 'parts':number}]
+    # the required datatype is:
+    # [{'color': string, 'name':string, 'parts':number}]
     recipe = Column(String(180), nullable=False)
 
     '''
@@ -65,7 +65,8 @@ class Drink(db.Model):
 
     def short(self):
         print(json.loads(self.recipe))
-        short_recipe = [{'color': r['color'], 'parts': r['parts']} for r in json.loads(self.recipe)]
+        short_recipe = [{'color': r['color'],
+                        'parts': r['parts']} for r in json.loads(self.recipe)]
         return {
             'id': self.id,
             'title': self.title,
