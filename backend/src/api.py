5b7a245de02e78642a5aa8154931542e0334b531
Omarb28 <omarb28@gmail.com>
2021-08-08 00:55:17 +0300

    Add application error handlers

--- backend/src/api.py
+++ backend/src/api.py
@@ -19,16 +19,37 @@ CORS(app)
 '''
 # db_drop_and_create_all()
 
+
+# Route for Testing
+@app.route('/login-results')
+def login_esults():
+    logout_url = 'https://omar-fsnd.us.auth0.com/v2/logout?client_id=z4tYXHIDAtht6BCDzGoAa5Ut0wyFZzYs&returnTo=http://localhost:5000/logout'
+    script = '<script>token = window.location.href.split("access_token=")[1].split("&")[0]; document.getElementById("demo").innerHTML = "Access Token is: " + token;</script>'
+    return f'<p id="demo">{script}</p><a href="{logout_url}">Logout</a>'
+
+
+# Route for Testing
+@app.route('/logout')
+def logout():
+    login_url = 'https://omar-fsnd.us.auth0.com/authorize?audience=coffeeshop&response_type=token&client_id=z4tYXHIDAtht6BCDzGoAa5Ut0wyFZzYs&redirect_uri=http://localhost:5000/login-results'
+    return f'<a href="{login_url}">Go to Login</a>'
+
+
 # ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks}
-        where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+
+@app.route('/drinks')
+@requires_auth('get:drinks')
+def retrieve_drinks(jwt):
+    '''
+    @TODO implement endpoint
+        GET /drinks
+            it should be a public endpoint
+            it should contain only the drink.short() data representation
+        returns status code 200 and json {"success": True, "drinks": drinks}
+            where drinks is the list of drinks
+            or appropriate status code indicating reason for failure
+    '''
+    return 'Get me a drink'
 
 
 '''
@@ -92,12 +113,12 @@ def unprocessable(error):
     return jsonify({
         "success": False,
         "error": 422,
-        "message": "unprocessable"
+        "message": "Unprocessable Entity"
     }), 422
 
 
 '''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
+@DONE implement error handlers using the @app.errorhandler(error) decorator
     each error handler should return (with approprate messages):
              jsonify({
                     "success": False,
@@ -107,13 +128,63 @@ def unprocessable(error):
 
 '''
 
+@app.errorhandler(405)
+def not_allowed(error):
+    return jsonify({
+        'success': False,
+        'error': 405,
+        'message': 'Method Not Allowed'
+    }), 405
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return jsonify({
+        'success': False,
+        'error': 500,
+        'message': 'Internal Server Error'
+    }), 500
+
+
 '''
-@TODO implement error handler for 404
+@DONE implement error handler for 404
     error handler should conform to general task above
 '''
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 404,
+        'message': 'Not Found'
+    }), 404
+
+
 '''
-@TODO implement error handler for AuthError
+@DONE implement error handler for AuthError
     error handler should conform to general task above
 '''
+
+
+@app.errorhandler(AuthError)
+def auth_error_handler(error):
+    status_code = error.status_code
+    error_code = error.error['code']
+    error_description = error.error['description']
+
+    messages = {
+        400: "Bad Request",
+        401: "Unauthorized",
+        403: "Forbidden"
+    }
+
+    status_code_message = messages.get(status_code, "Status code message could not be found")
+
+    return jsonify({
+        "success": False,
+        "error": status_code,
+        "message": status_code_message,
+        "code": error_code,
+        "description": error_description
+    }), status_code
